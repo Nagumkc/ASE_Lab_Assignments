3ac52f4d5740de22157bc15cbcdca203b950f8c2
Nagumkc <nandigam.nag@gmail.com>
2017-03-21 22:26:41 -0500

    commit

--- Lab 9/Source/Lab 9/Server.js
+++ Lab 9/Source/Lab 9/Server.js
@@ -4,13 +4,17 @@
 
 var express = require('express');
 var app = express();
+var cors=require('cors');
 var request = require('request');
-app.get('/calories', function (req, res) {
-    var result = {
-        'calo': []
-    };
+app.use(cors());
 
-    request('https://api.edamam.com/api/nutrition-data?app_id=254d36ea&app_key=383e71430a323ae94ec781cab4b8918a&ingr=1%20large%20apple', function (error, response, body) {
+app.get('/calories', function (req, res,next) {
+    var result = {
+"calo":[]
+    };
+var z=req.query.name;
+console.log(z);
+    request("https://api.edamam.com/api/nutrition-data?app_id=254d36ea&app_key=383e71430a323ae94ec781cab4b8918a&ingr=1%20large%20"+z, function (error, response, body) {
         //Check for error
         if (error) {
             return console.log('Error:', error);
@@ -19,18 +23,36 @@ app.get('/calories', function (req, res) {
             return console.log('Invalid Status Code Returned:', response.statusCode);
         }
         body = JSON.parse(body);
-        result.calo.push(body.calories);
-        res.contentType('application/json');
-        res.write(JSON.stringify(result));
-        res.end();
+        result.calo.push({"calories":body.calories,"quantity":body.totalWeight});
+
+   //   res.contentType('application/json');
+   //  res.write(JSON.stringify(result));
+   //  res.end();
+        request("https://kgsearch.googleapis.com/v1/entities:search?query="+z+"&key=AIzaSyA1Sy0t-uNoi-9pnR5ETJnTBGDNvoi6YhE&limit=1&indent=True",function (error1, response1, body1) {
+            if (error1) {
+                return console.log('Error:', error1);
+            }
+            if (response1.statusCode !== 200) {
+                return console.log('Invalid Status Code Returned:', response1.statusCode);
+            }
+            body = JSON.parse(body1);
+            result.calo.push({"link": body.itemListElement[0].result.image.contentUrl})
+            res.contentType('application/json');
+            res.write(JSON.stringify(result));
+            res.s
+            res.end();
+        });
     });
     console.log(result);
-}
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
 
-    console.log("Example app listening at http://%s:%s", host, port)
+
+
+})
+var server = app.listen(8082, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port);
 })
 
 
---
+++ Lab 9/Source/Lab 9/app.js
@@ -0,0 +1,29 @@
+/**
+ * Created by VenkatNag on 3/18/2017.
+ */
+angular.module("ap",[])
+.controller('con',function ($scope,$http) {
+    $scope.search=function () {
+
+        $http.get("http://127.0.0.1:8082/calories",{params: {name:$scope.in}}).success(function (data) {
+
+           if (data != null) {
+               $scope.final= {
+                   "score": data.calo[0].calories,
+                   "image": data.calo[1].link,
+                   "qt":data.calo[0].quantity
+               }
+           }
+        });
+           document.getElementById("col").style.display='block';
+
+        /*handler.(function (data) {
+            if (data != null) {
+                $scope.final=data.calo[0].calories +" "+data.calo[1].link;
+            }
+        })
+        handler.error(function (data) {
+            alert("There was some error processing your request. Please try after some time.");
+        })*/
+    }
+})
